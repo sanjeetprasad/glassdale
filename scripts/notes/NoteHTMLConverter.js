d9a15c97b35485b9d11292d0de07e48c03534bb7
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2020-11-09 12:59:01 -0600

    added the delete button in NoteHTMLConverter.js, fetch the delete data from API and add an event listner the button.

--- scripts/notes/NoteHTMLConverter.js
+++ scripts/notes/NoteHTMLConverter.js
@@ -6,6 +6,7 @@ export const NoteAsHTML = (noteObject) => {
     <p>Date of Interview: ${noteObject.dateOfInterview}</p>
     <p>Time Note Entered: ${new Date(noteObject.timestamp).toLocaleDateString('en-US')}</p>
     <p>Note: ${noteObject.note}</p>
+    <button id="deleteNote--${noteObject.id}">Delete</button>
     
     </div>
     `
--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,6 +1,6 @@
 
 import { NoteAsHTML } from './NoteHTMLConverter.js'
-import { getNotes, useNotes} from "./NotesDataProvider.js"
+import { getNotes, useNotes, deleteNote} from "./NotesDataProvider.js"
 import {getCriminals, useCriminals} from "../criminals/CriminalProvider.js"
 
 // get the notes from the api >> use the notes array
@@ -35,8 +35,29 @@ const render = (notesArray, crimainalsArray) => {
     <h3>Case Notes</h3>
     ${notesHTMLRepresentations}
     `
-
 }
+
+eventHub.addEventListener("click", clickEvent => {
+
+    // console.log(clickEvent, "id?")
+    if (clickEvent.target.id.startsWith("deleteNote--")) {
+        const [prefix, id] = clickEvent.target.id.split("--")
+
+        /*
+            Invoke the function that performs the delete operation.
+
+            Once the operation is complete you should THEN invoke
+            useNotes() and render the note list again.
+        */
+       deleteNote(id).then(
+           () => {
+               const updatedNotes = useNotes()
+               const criminals = useCriminals()
+               render(updatedNotes, criminals)
+           }
+       )
+    }
+})
 
 
 
